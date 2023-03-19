748d2364d76628a05dbde38f5dac434f129fd051
Adam Dutson <Quibly@byui.edu>
2023-03-18 23:08:26 -0600

    Updated puzzle and gamer management

---
+++ game/data/database.py
@@ -0,0 +1,84 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def createDatabase():
+    # Use a service account.
+    cred = credentials.Certificate('./game/data/hangman.json')
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
+
+db = createDatabase()
+
+#call a database reference
+class Database:
+    def __init__(self):
+        self.dataset = db
+
+    # Method to add data to a collection
+    def get_data(self, collection):
+        if collection == 'users':
+            reference = self.dataset.collection(u'users')
+        elif collection == 'puzzles':
+            reference = self.dataset.collection(u'puzzles')
+        docs = reference.stream()
+        return docs
+        #for doc in docs:
+        #    print(f'{doc.id} => {doc.to_dict()}')
+
+    # Method to add data to a collection
+    def add_data(self, collection, value=''):
+        if collection == 'users':
+            gamertag = value
+            reference = self.dataset.collection(u'users').document()
+            reference.set({
+                u'gamertag': gamertag,
+                u'rank': u'Beginner',
+                U'puzzles': []
+            })
+        elif collection == 'puzzles':
+            puzzle = input('Please enter the new puzzle string: ')
+            reference = self.dataset.collection(u'puzzles').document()
+            reference.set({
+                u'puzzle': puzzle
+            })
+
+    # Method to change the gamertag of the user in the database
+    def change_gamertag(self, existingGamertag, newGamertag):
+        id = self.get_user(existingGamertag)
+        self.dataset.collection(u'users').document(id).update({u'gamertag': newGamertag})
+
+    # Method to update the puzzle history on gamer details in database
+    def change_puzzles(self, gamertag, puzzle):
+        gamerId = self.get_user(gamertag)
+        docs = self.get_data('puzzles')
+        for doc in docs:
+            dictionary = doc.to_dict()
+            p = dictionary['puzzles']
+            if puzzle == p:
+                puzzleId = doc.id
+                gamer = self.dataset.collection(u'users').document(gamerId).get()
+                gamerDict = gamer.to_dict()
+                gamerpuzzles = gamerDict['puzzles']
+                if gamerpuzzles == None:
+                    self.dataset.collection(u'users').document(gamerId).update({u'puzzles': puzzleId})
+                else:
+                    self.dataset.collection(u'users').document(gamerId).update({u'puzzles': firestore.ArrayUnion([puzzleId])})
+
+    # Method to delete user
+    def delete_data(self, gamertag):
+        id = self.get_user(gamertag)
+        self.dataset.collection(u'users').document(id).delete()
+
+    # Method to get user id
+    def get_user(self, gamertag):
+        docs = self.get_data('users')
+        for doc in docs:
+            dictionary = doc.to_dict()
+            gt = dictionary['gamertag']
+            if gt == gamertag:
+                return doc.id
+
+
+        
--- game/pythonHangman.py
+++ game/pythonHangman.py
@@ -1,14 +1,19 @@
+from game.data.database import Database
+import random
+
 class Display:
     def __init__(self):
         self.maxGuesses = 7
         self.missedLetters = 0
         self.incorrectGuesses = 1
-        self.phrase = "The sum of aegis"
+        self.database = Database()
+        self.phrase = self.get_puzzle()
         self.lettersGuessed = []
+        
 
     #Function that houses the progress of the program by controlling loops
     #for gameplay and checks for game completion
-    def control(self):
+    def control(self, gamertag):
         display1 = Display()
         # While the player hasn't missed too many guesses or completed the puzzle.
         # they will be able to keep guessing
@@ -29,6 +34,7 @@ class Display:
             # Otherwise, if they miss too many guesses they lose.
             if (display1.missedLetters == 0):
                 display1.incorrectGuesses = display1.maxGuesses
+                self.database.change_puzzles(gamertag, self.phrase)
                 print("You got all of the letters! You won!")
                 print()
             elif (display1.incorrectGuesses == display1.maxGuesses):
@@ -46,7 +52,7 @@ class Display:
             print(displayArray[i-1])
         print()
         # Loop to display the guessed and missed letters of the puzzle.
-        for i in range(1,len(display.phrase)):
+        for i in range(1,len(display.phrase)+1):
             letter = display.phrase[i-1]
             if (letter.lower() in display.lettersGuessed):
                 print(letter, end =" ")
@@ -90,6 +96,42 @@ class Display:
             display.lettersGuessed.append(guess.lower())
             display.incorrectGuesses += 1
         return self.createDisplay(display)
+    
+    # Function to retrieve a random puzzle from the Firebase database
+    def get_puzzle(self):
+        docs = self.database.get_data('puzzles')
+        num = int(random.randrange(1,5))
+        for doc in docs:
+            if int(doc.id) == num:
+                dictionary = doc.to_dict()
+                puzzle = dictionary['puzzle']
+                return puzzle
+    # Function to check if gamertag is already created
+    def is_user(self, gamertag):
+        docs = self.database.get_data('users')
+        for doc in docs:
+            dictionary = doc.to_dict()
+            gt = dictionary['gamertag']
+            if gt == gamertag:
+                return True
+        return False
+    
+    # Function to give user options to change or delete gamertag
+    def menu(self, gamertag):
+        option = 0
+        while (option !=3):
+            option = int(input("""
+Push '1' to Change Gamertag 
+Push '2' to Delete Gamertag
+Push '3' to Play
+"""))
+            if option == 1:
+                newtag = input('What would you like your new gamertag to be? ')
+                self.database.change_gamertag('users', gamertag, newtag)
+            elif option == 2:
+                self.database.delete_data(gamertag)
+                newtag = input('What would you like your new gamertag to be? ')
+                self.database.add_data('users', newtag)
 
     # Function to start the program and house control function.
     def main(self):
@@ -97,4 +139,8 @@ class Display:
         print("")
         print("Phrases are generated automatically.")
         print("")
-        self.control()
+        gamertag = input('Please enter your gamertag: ')
+        if not self.is_user(gamertag):
+            self.database.add_data('users', gamertag)
+        self.menu(gamertag)
+        self.control(gamertag)
